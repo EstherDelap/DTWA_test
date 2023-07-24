e4498bafb169c978d199bc41087c764cd16dfba6
EstherDelap <py20ed@leeds.ac.uk>
2023-07-24 16:11:48 +0100

    increased N and number_repeats

--- main.jl
+++ main.jl
@@ -4,8 +4,8 @@ using JLD2
 function main()
     #commit_id = readchomp(`git rev-parse HEAD`)
     dim = (4,4,4)
-    N = 1000
-    number_repeats = 100
+    N = 10000
+    number_repeats = 1000
     Γ_deph = 0
     Γ_decay = 0.0025
     Ω = 0 
