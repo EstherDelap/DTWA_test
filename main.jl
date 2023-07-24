debb077cf945ba8931ace37143c3322e83816d4b
EstherDelap <py20ed@leeds.ac.uk>
2023-07-24 15:10:00 +0100

    increased number of time steps and number of repeats

--- main.jl
+++ main.jl
@@ -4,8 +4,8 @@ using JLD2
 function main()
     #commit_id = readchomp(`git rev-parse HEAD`)
     dim = (4,4,4)
-    N = (1)
-    number_repeats = 10
+    N = 1000
+    number_repeats = 100
     Γ_deph = 0
     Γ_decay = 0.0025
     Ω = 0 
