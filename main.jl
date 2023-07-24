a47bd5da00cd617c6a0613dfbe7e9e15d433b56a
EstherDelap <py20ed@leeds.ac.uk>
2023-07-24 17:52:53 +0100

    increased repeats

--- main.jl
+++ main.jl
@@ -5,15 +5,14 @@ function main()
     #commit_id = readchomp(`git rev-parse HEAD`)
     dim = (5,4,2)
     N = 10000
-    number_repeats = 1000
+    number_repeats = 10000
     Γ_deph = 0.2
     Γ_decay = 0
     Ω = 5
     α = 0
-    axis = 3
 
     outfile = "Results_3.jld2"
-    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, "Ising", axis)
+    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, "Ising")
     jldsave(outfile; collective_spin = rv1, average = rv2)
 end
 
