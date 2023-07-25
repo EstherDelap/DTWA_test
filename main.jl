30171723def6d83367432000e4bfdd629ff589e2
EstherDelap <py20ed@leeds.ac.uk>
2023-07-25 16:17:30 +0100

    attempting to use a job array

--- main.jl
+++ main.jl
@@ -1,19 +1,22 @@
 using DTWA
 using JLD2
 
-function main()
-    #commit_id = readchomp(`git rev-parse HEAD`)
-    dim = (4,4,4)
-    N = 10000
-    number_repeats = 10000
-    Γ_deph = 0
-    Γ_decay = 0.0025
-    Ω = 0
-    α = 6
+#dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, method = split(read(ARGS[1], String), r"\s+")
 
-    outfile = "Results_newer.jld2"
-    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, "Ising")
-    jldsave(outfile; collective_spin = rv1, average = rv2)
+function main(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, method, axis, dir)
+    #commit_id = readchomp(`git rev-parse HEAD`)
+    #dim = (4,4,4)
+    #N = 10000
+    #number_repeats = 10000
+    #Γ_deph = 0
+    #Γ_decay = 0.0025
+    #α = 6
+    #Ω = 0
+    #method = "Ising"
+
+    outfile = "results.jld2"
+    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, method, axis, dir)
+    jldsave(outfile; α=α, Γ_deph=Γ_deph, Γ_decay=Γ_decay, Ω = Ω, axis = axis, dir = dir, collective_spin = rv1, average = rv2)
 end
 
-main()
+main(ARGS[1],ARGS[2], ARGS[3], ARGS[4], ARGS[5], ARGS[6], ARGS[7], ARGS[8], ARGS[9], ARGS[10])
--- submit.sh
+++ submit.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -l
 
 # Wall clock time
-#$ -l h_rt=24:00:00
+#$ -l h_rt=72:00:00
 
 # Request a single core
 #$ -pe smp 1
 
 # Request RAM
-#$ -l mem=100G
+#$ -l mem=150G
 
 # Request TMPDIR space
-#$ -l tmpfs=20G
+#$ -l tmpfs=500G
+
+
+# Set up the job array.  In this instance we have requested 10000 tasks
+# numbered 1 to 10000.
+#$ -t 1-10
 
 #$ -o output
 
@@ -22,15 +27,29 @@
 # Job name
 #$ -N OTHER
 
+# Parse parameter file to get variables.
+paramfile=$BASE/input/params.txt
+number=$SGE_TASK_ID
 
 BASE=$PWD
+#LOAD=$BASE/input
 SAVE=$BASE/results
 
 module load julia/1.9.1
 
-number=$SGE_TASK_ID
+filename="`sed -n ${number}p $paramfile | awk '{print $1}'`"
+v1="`sed -n ${number}p $paramfile | awk '{print $2}'`" #dim
+v2="`sed -n ${number}p $paramfile | awk '{print $3}'`" #N
+v3="`sed -n ${number}p $paramfile | awk '{print $4}'`" #number_repeats
+v4="`sed -n ${number}p $paramfile | awk '{print $5}'`" #Γ_deph 
+v5="`sed -n ${number}p $paramfile | awk '{print $6}'`" #Γ_decay
+v6="`sed -n ${number}p $paramfile | awk '{print $7}'`" #Ω
+v7="`sed -n ${number}p $paramfile | awk '{print $8}'`" #α
+v8="`sed -n ${number}p $paramfile | awk '{print $9}'`" #method 
+v9="`sed -n ${number}p $paramfile | awk '{print $10}'`" #axis 
+v10="`sed -n ${number}p $paramfile | awk '{print $11}'`" #dir 
 
 cd $TMPDIR
 cp $BASE/main.jl $TMPDIR/
-julia --project=$BASE main.jl
-mv Results_newer.jld2 $SAVE/results_2.jld2
+julia --project=$BASE main.jl $v1 $v2 $v3 $v4 $v5 $v6 $v7 $v8 $v9 $v10
+mv results.jld2 $SAVE/$filename 
