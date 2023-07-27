4168a564b8508ad36d6e94c554cd444664ced96c
EstherDelap <py20ed@leeds.ac.uk>
2023-07-27 12:24:29 +0100

    added a try cath loop to try and make alpha an integer for the disipative model and a float for the XYZ model

--- main.jl
+++ main.jl
@@ -2,11 +2,20 @@ using DTWA
 using JLD2
 
 
-function main(dim, N, number_repeats, Γ_deph, Γ_decay, Ω, α, method, axis, dir)
-
+function main(dim, N, number_repeats, Γ_deph, Γ_decay, Ω, α1,α2, α3, method, axis, dir)
+    try 
+        a = parse(Int64,α1)
+        b = parse(Int64,α2)
+        c = parse(Int64,α3)
+    catch
+        a = parse(Float64,α1)
+        b = parse(Float64,α2)
+        c = parse(Float64,α3)
+    end
     outfile = "results.jld2"
-    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, method, axis, dir)
+    rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, [a,b,c], method, axis, dir)
     jldsave(outfile; α=α, Γ_deph=Γ_deph, Γ_decay=Γ_decay, Ω = Ω, axis = axis, dir = dir, collective_spin = rv1, average = rv2)
 end
 
-main([parse(Int64,ARGS[1]),parse(Int64,ARGS[2]),parse(Int64,ARGS[3])],parse(Int64,ARGS[4]),parse(Int64,ARGS[5]), parse(Float64,ARGS[6]), parse(Float64,ARGS[7]), parse(Float64,ARGS[8]), [parse(Float64,ARGS[9]), parse(Float64,ARGS[10]),parse(Float64,ARGS[11])], ARGS[12], parse(Int64,ARGS[13]), parse(Int64,ARGS[14]))
+
+main([parse(Int64,ARGS[1]),parse(Int64,ARGS[2]),parse(Int64,ARGS[3])],parse(Int64,ARGS[4]),parse(Int64,ARGS[5]), parse(Float64,ARGS[6]), parse(Float64,ARGS[7]), parse(Float64,ARGS[8]), ARGS[9], ARGS[10], ARGS[11], ARGS[12], parse(Int64,ARGS[13]), parse(Int64,ARGS[14]))
