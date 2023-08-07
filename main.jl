f5a56dc735e5090c339d8c554d2d805411044e42
EstherDelap <py20ed@leeds.ac.uk>
2023-08-07 09:41:53 +0100

    syntax error

--- main.jl
+++ main.jl
@@ -3,7 +3,6 @@ using JLD2
 
 
 function main(dim, N, number_repeats, Γ_deph, Γ_decay, Ω, α1,α2, α3, method, axis, dir)
-    function main(dim)
     if method === "Ising"
         a = parse(Int64,α1)
         b = parse(Int64,α2)
@@ -16,8 +15,6 @@ function main(dim, N, number_repeats, Γ_deph, Γ_decay, Ω, α1,α2, α3, metho
     outfile = "results.jld2"
     rv1, rv2= DTWA.repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, [a,b,c], method, axis, dir)
     jldsave(outfile;dim=dim, α=[a,b,c], Γ_deph=Γ_deph, Γ_decay=Γ_decay, Ω = Ω, axis = axis, dir = dir, collective_spin = rv1, average = rv2)
-    #jldsave(outfile; dim=dim)
 end
 
 main([parse(Int64,ARGS[1]),parse(Int64,ARGS[2]),parse(Int64,ARGS[3])],parse(Int64,ARGS[4]),parse(Int64,ARGS[5]), parse(Float64,ARGS[6]), parse(Float64,ARGS[7]), parse(Float64,ARGS[8]), ARGS[9], ARGS[10], ARGS[11], ARGS[12], parse(Int64,ARGS[13]), parse(Int64,ARGS[14]))
-#main([parse(Int64,ARGS[1]),parse(Int64,ARGS[2]),parse(Int64,ARGS[3])])
