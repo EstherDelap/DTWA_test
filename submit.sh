9130e979dab61cb76774c731eeaeaccd3b4c38ba
EstherDelap <py20ed@leeds.ac.uk>
2023-08-07 16:56:10 +0100

    sadf

--- submit.sh
+++ submit.sh
@@ -24,8 +24,8 @@
 # Set working dir
 #$ -wd /home/ucanede/Scratch/dissipative_model_2
 
-# Job name
-#$ -N XYZ
+# Job name 
+#$ -N Ising
 
 # Parse parameter file to get variables.
 number=$SGE_TASK_ID
@@ -37,27 +37,26 @@ SAVE=$Base/results
 
 module load julia/1.9.1
 filename="`sed -n ${number}p $paramfile | awk '{print $1}'`"
-#filename="`sed -n ${number}p $paramfile | awk '{print $1}'`"
-#v1="`sed -n ${number}p $paramfile | awk '{print $2}'`" #dim
-#v2="`sed -n ${number}p $paramfile | awk '{print $3}'`" #dim
-#v3="`sed -n ${number}p $paramfile | awk '{print $4}'`" #dim
+v1="`sed -n ${number}p $paramfile | awk '{print $2}'`" #dim
+v2="`sed -n ${number}p $paramfile | awk '{print $3}'`" #dim
+v3="`sed -n ${number}p $paramfile | awk '{print $4}'`" #dim
 
-#v4="`sed -n ${number}p $paramfile | awk '{print $5}'`" #N
-#v5="`sed -n ${number}p $paramfile | awk '{print $6}'`" #number_repeats
-#v6="`sed -n ${number}p $paramfile | awk '{print $7}'`" #Γ_deph 
-#v8="`sed -n ${number}p $paramfile | awk '{print $9}'`" #Ω
-#v7="`sed -n ${number}p $paramfile | awk '{print $8}'`" #Γ_decay
+v4="`sed -n ${number}p $paramfile | awk '{print $5}'`" #N
+v5="`sed -n ${number}p $paramfile | awk '{print $6}'`" #number_repeats
+v6="`sed -n ${number}p $paramfile | awk '{print $7}'`" #Γ_deph 
+v8="`sed -n ${number}p $paramfile | awk '{print $9}'`" #Ω
+v7="`sed -n ${number}p $paramfile | awk '{print $8}'`" #Γ_decay
 
-#v9="`sed -n ${number}p $paramfile | awk '{print $10}'`" #α
-#v10="`sed -n ${number}p $paramfile | awk '{print $11}'`" #α 
-#v11="`sed -n ${number}p $paramfile | awk '{print $12}'`" #α
+v9="`sed -n ${number}p $paramfile | awk '{print $10}'`" #α
+v10="`sed -n ${number}p $paramfile | awk '{print $11}'`" #α 
+v11="`sed -n ${number}p $paramfile | awk '{print $12}'`" #α
 
-#v12="`sed -n ${number}p $paramfile | awk '{print $13}'`" #method
-#v13="`sed -n ${number}p $paramfile | awk '{print $14}'`" #axis 
-#v14="`sed -n ${number}p $paramfile | awk '{print $15}'`" #dir
+v12="`sed -n ${number}p $paramfile | awk '{print $13}'`" #method
+v13="`sed -n ${number}p $paramfile | awk '{print $14}'`" #axis 
+v14="`sed -n ${number}p $paramfile | awk '{print $15}'`" #dir
 
 cd $TMPDIR
 cp $BASE/main.jl $TMPDIR/
-#julia --project=$BASE main.jl $v1 $v2 $v3 $v4 $v5 $v6 $v7 $v8 $v9 $v10 $v11 $v12 $v13 $v14
-julia --project=$BASE main.jl "4" "4" "4" "100" "100" "0.0" "0.0025" "0" "0" "0" "0" "Ising" "1" "1"
-mv results.jld2 /home/ucanede/Scratch/dissipative_model_2/results/$filename
+julia --project=$BASE main.jl $v1 $v2 $v3 $v4 $v5 $v6 $v7 $v8 $v9 $v10 $v11 $v12 $v13 $v14
+
+mv results.jld2 /home/ucanede/Scratch/dissipative_model_2/results/results_Ising/$filename
